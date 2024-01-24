4086b427bcc73b233777b45f64a30c5053399cb6
Radim Kolar <radimkolar22@gmail.com>
2024-01-24 11:52:23 +0100

    - change app structure - change locale text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     template: `
-        <div class="app w-full h-full bg-slate-50">
-            <div class="flex items-center justify-center">
-                <app-birth-code
-                    class="bg-violet text-white p-12 rounded-3xl min-w-[680px] min-h-[360px] relative flex flex-col justify-between"
-                />
-            </div>
+        <div
+            class="app w-full h-full bg-white absolute left-0 top-0 flex items-center justify-center"
+        >
+            <app-birth-code
+                class="bg-violet text-white p-12 rounded-3xl min-w-[680px] min-h-[360px] relative flex flex-col justify-between shadow-2xl"
+            />
         </div>
     `,
 })
